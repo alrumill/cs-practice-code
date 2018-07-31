6f030b9c01b78f3aed9c7d99e9b871e64069f4dc
Allen Miller <allen.miller@ad.infosys.com>
2018-07-31 14:13:41 -0700

    initial push

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdvancedMVCApplication.Models;
+
+namespace AdvancedMVCApplication.Controllers
+{
+
+    public class UserController : Controller
+    {
+        private static Users _users = new Users();
+
+        //Action for Index View  
+        public ActionResult Index()
+        {
+            return View(_users.UserList);
+        }
+
+        //Action for UserAdd View 
+        [HttpGet]
+        public ActionResult UserAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UserAdd(UserModels userModel)
+        {
+            _users.CreateUser(userModel);
+            return View("Index", _users.UserList);
+        }
+
+        //Action for Details View 
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            return View(_users.UserList.FirstOrDefault(x => x.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult Details()
+        {
+            return View("Index", _users.UserList);
+        }
+
+        //Action for Edit View 
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(_users.UserList.FirstOrDefault(x => x.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(UserModels userModel)
+        {
+            _users.UpdateUser(userModel);
+            return View("Index", _users.UserList);
+        }
+
+        //Action for Delete View 
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            return View(_users.UserList.FirstOrDefault(x => x.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(UserModels userModel)
+        {
+            _users.DeleteUser(userModel);
+            return View("Index", _users.UserList);
+        }
+    }
+}
