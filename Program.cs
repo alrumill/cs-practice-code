ab139d13098a5d265639e7f6525696b01db00952
Allen Miller <allen.miller@ad.infosys.com>
2018-07-31 17:07:08 -0400

    initial push

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Some some stuff in.");
+            var a = Console.ReadLine();
+            Console.WriteLine(a);
+            if (a == "hi")
+            {
+                for (int i =0; i<a.Length; i++) {
+                    Console.WriteLine("k");
+                }
+                Console.WriteLine("k");
+            }
+            else
+            {
+                foreach (char item in a) {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
